1de0228faa9c20ae45d65511891d3e30b2bb4e19
Mithun <mithunmitz.12@gmail.com>
2025-07-21 14:46:38 +0530

    Cors origin config

--- banking-backend-microservices/portfolio-service/portfolio-service/src/main/java/com/cib/portfolio/controller/PortfolioController.java
+++ banking-backend-microservices/portfolio-service/portfolio-service/src/main/java/com/cib/portfolio/controller/PortfolioController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cib.portfolio.entity.Portfolio;
 import com.cib.portfolio.entity.Dtos.PortfolioHistoryDto;
@@ -16,6 +13,7 @@ import com.cib.portfolio.service.PortfolioService;
 
 @RestController
 @RequestMapping("/api/portfolio")
+@CrossOrigin
 public class PortfolioController {
 
     @Autowired
